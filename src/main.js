561fff7f7935e169416d22f5fcb39bbf336eb899
feyaq13 <feyaq13@gmail.com>
2020-05-28 12:17:04 +0300

    добавляет перечень услуг и реализует логику для карусели

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import 'regenerator-runtime/runtime'
+
 const menuBurger = document.querySelector('.nav__btn-toggle');
 const navList = document.querySelector('.nav-list');
 
@@ -11,3 +13,35 @@ menuBurger.addEventListener('click', function () {
   document.querySelector('.header__logo').classList.toggle('active-menu');
   document.body.classList.toggle('body--locked');
 });
+
+const carousel = document.getElementsByClassName('section-catalog__carousel')[0];
+const cells = carousel.children;
+
+window.onload = function () {
+
+  (async function autoplay() {
+
+    let prevCell;
+    for (let i = 0; i < cells.length;) {
+
+      await new Promise((resolve) => {
+        setTimeout(resolve, 1000);
+      });
+
+      if (prevCell) {
+        prevCell.classList.remove('carousel-cell--active');
+      }
+
+      cells[i].classList.add('carousel-cell--active')
+      prevCell = cells[i];
+
+      if (i === cells.length - 1) {
+        cells[i].classList.add('carousel-cell--active');
+        i = 0;
+        continue
+      }
+
+      i++
+    }
+  })();
+};
